d558c5da8592e1081ce6b3292ad0242bd6cc6f86
Amaiyo Praises <amaiyo.praises@gmail.com>
2024-09-17 17:47:25 +0100

    Modified - Fixed a bug

---
+++ index.mjs
@@ -0,0 +1,26 @@
+// const jsonServer = require('json-server');
+
+// const server = jsonServer.create();
+// const router = jsonServer.router('db.json');
+// const middlewares = jsonServer.defaults();
+// const port = process.env.PORT || 8080;
+
+// server.use(middlewares);
+// server.use(router);
+
+// server.listen(port);
+
+import jsonServer from 'json-server'; // importing json-server
+import cors from 'cors'; // importing cors for cross-origin requests
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // point to your db.json file
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 8080; // set your port, e.g., 8080
+
+server.use(cors()); // enable cors
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
